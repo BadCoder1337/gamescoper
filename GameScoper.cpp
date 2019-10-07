3740505daaffc49fc09ad62a513cc4bc70f5abdc
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-10-07 15:50:22 +0300

    Zoom scaling

--- GameScoper.cpp
+++ GameScoper.cpp
@@ -3,11 +3,9 @@
 #include "iostream"
 #include "fstream"
 #include "strsafe.h"
-//основная либа
 #include "magnification.h"
 #include "signal.h"
 
-//без этого она не заводится
 #pragma comment (lib, "magnification.lib")
 
 using namespace std;
@@ -16,14 +14,14 @@ HHOOK mouseHook;
 HHOOK _hook_keyboard;
 KBDLLHOOKSTRUCT kbdStruct;
 
-bool active; //активен ли зум
-bool pressed; //нажат ли прицел
-int cScope; //текущий коэффициент увеличения
-float scopes[4]; //4 слота для прицеливания
+bool active;
+bool pressed;
+int cScope;
+float scopes[4];
 char activationButton;
 char cyclingButton;
 
-void getCfg() //загружаем конфиг
+void GetCfg()
 {
 	char string[20];
 	ifstream fin("config.txt");
@@ -43,43 +41,40 @@ void getCfg() //загружаем конфиг
 	fin.close();
 }
 
-void SetZoom(float magnificationFactor) //установка зума
-{
-	if (magnificationFactor >= 1.0)
-	{
+BOOL SetZoom(float magnificationFactor) {
+	if (magnificationFactor >= 1.0) {
 		int xDlg = (int)((float)GetSystemMetrics(SM_CXSCREEN) * (1.0 - (1.0 / magnificationFactor)) / 2.0);
 		int yDlg = (int)((float)GetSystemMetrics(SM_CYSCREEN) * (1.0 - (1.0 / magnificationFactor)) / 2.0);
-		MagSetFullscreenTransform(magnificationFactor, xDlg, yDlg);
-		BOOL fSuccess = MagSetFullscreenTransform(magnificationFactor, xDlg, yDlg);
-		if (!fSuccess)
-		{
-			cout << "failed to magnify screen!" << endl;
-		}
-		else
-		{
-			cout << "zoomed to: " << magnificationFactor << endl;
-		}
+		return MagSetFullscreenTransform(magnificationFactor, xDlg, yDlg);
+	} else {
+		return false;
+	}
+}
+
+void ScaleZoom(float from, float to, float time = 100) {
+	for (int i = 0; i <= 60; i++) {
+		SetZoom(from + (to - from) * i / 60);
+		Sleep(time / 60);
 	}
 }
 
 LRESULT __stdcall MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam) { //хук на мышь
-	if (nCode >= 0)
-	{
-		switch (wParam)
-		{
+	if (nCode >= 0) {
+		switch (wParam) {
 		case WM_RBUTTONDOWN:
-			cout << "right button down" << endl;
 			pressed = 1;
-			if (active)
-			{
-				SetZoom(scopes[cScope]);
+			if (active) {
+				cout << "right button down" << endl;
+				ScaleZoom(1, scopes[cScope]);
 			}
 			break;
 
 		case WM_RBUTTONUP:
-			cout << "right button up" << endl;
 			pressed = 0;
-			SetZoom(1);
+			if (active) {
+				cout << "right button up" << endl;
+				ScaleZoom(scopes[cScope], 1);
+			}
 			break;
 		}
 	}
@@ -87,38 +82,34 @@ LRESULT __stdcall MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam) { /
 }
 
 LRESULT __stdcall HookCallbackKeyboard(int nCode, WPARAM wParam, LPARAM lParam) { //хук на клавиатуру
-	if (nCode >= 0)
-	{
+	if (nCode >= 0) {
 		kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
 		if (wParam == WM_KEYDOWN) {
 			char c = MapVirtualKey(kbdStruct.vkCode, 2);
-			if (c == activationButton)
-			{
-				if (active) 
-				{
+			if (c == activationButton) {
+				if (active) {
 					cout << "zoom is deactivated" << endl;
-					SetZoom(1); 
-				}
-				else
-				{
+					if (pressed) {
+						ScaleZoom(scopes[cScope], 1);
+					}
+				} else {
 					cout << "zoom is activated" << endl;
+					if (pressed) {
+						ScaleZoom(1, scopes[cScope]);
+					}
 				}
 				active = !active;
 			}
-			if (c == cyclingButton)
-			{
-				if (cScope < 3)
-				{
+			if (c == cyclingButton) {
+				int prevScope = cScope;
+				if (cScope < 3) {
 					cScope++;
-				}
-				else
-				{
+				} else {
 					cScope = 0;
 				}
 				cout << "selected scope: " << cScope + 1 << endl;
-				if (pressed && active)
-				{
-					SetZoom(scopes[cScope]);
+				if (pressed && active) {
+					ScaleZoom(scopes[prevScope], scopes[cScope]);
 				}
 			}
 		}
@@ -126,25 +117,22 @@ LRESULT __stdcall HookCallbackKeyboard(int nCode, WPARAM wParam, LPARAM lParam) 
 	return CallNextHookEx(_hook_keyboard, nCode, wParam, lParam);
 }
 
-void SetHook() { //активируем хуки
-	if (!(_hook_keyboard = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallbackKeyboard, NULL, 0)))
-	{
+void SetHook() {
+	if (!(_hook_keyboard = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallbackKeyboard, NULL, 0))) {
 		cout << "failed to install hook on keyboard!" << endl;
 	}
-	if (!(mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookCallback, NULL, 0)))
-	{
+	if (!(mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookCallback, NULL, 0))) {
 		cout << "failed to install mouse hook!" << endl;
 	}
 }
 
 int main()
 {
-	getCfg();
+	GetCfg();
 	MagInitialize();
 	SetHook();
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
+	while (GetMessage(&msg, NULL, 0, 0)){
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
